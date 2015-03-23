68c9e73a6feb6cd102c7d0c86f70430845dd6006
cyro809 <cyro809@gmail.com>
2015-03-22 23:29:23 -0300

    Ananagram faltando remover valores duplicados

--- ananagram.java
+++ ananagram.java
@@ -1,3 +1,11 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -11,10 +19,46 @@ public class ananagram {
 		String input = scan.nextLine();
 		String[] words = input.split(" ");
 		
+		HashMap<String, String>word_map = new HashMap<String, String>();
+		List<String> result = new ArrayList<String>();
+		List<String> alphabetic_words = new ArrayList<String>();
+		
 		for(int i=0;i<words.length;i++) {
-			System.out.println(words[i]);
+	        char[] chars = words[i].toLowerCase().toCharArray();
+	        Arrays.sort(chars);
+	        String sorted = new String(chars);
+	        
+	        alphabetic_words.add(sorted);
+			word_map.put(words[i], sorted);
 		}
-		scan.close();
-	}
+		
 
+		System.out.println(alphabetic_words);
+		Collections.sort(alphabetic_words);
+		System.out.println(alphabetic_words);
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+	    for (String w : alphabetic_words) {
+	        Integer n = map.get(w);
+	        n = (n == null) ? 1 : ++n;
+	        map.put(w, n);
+	    }
+	    
+		for(int i=0;i<map.size();i++) {
+			
+		}
+		
+		System.out.println(result);
+		scan.close();
+	
+	}	
+	public static Object getKeyFromValue(Map hm, Object value) {
+	    for (Object o : hm.keySet()) {
+	      if (hm.get(o).equals(value)) {
+	        return o;
+	      }
+	    }
+	    return null;
+	 }
+	
 }
