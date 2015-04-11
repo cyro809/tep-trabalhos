3304fe3c3bc4ec31add1c3cab9390f3b588eabc0
cyro809 <cyro809@gmail.com>
2015-04-11 12:40:54 -0300

    Solução para Common Permutation

--- commonpermutation.cpp
+++ commonpermutation.cpp
@@ -13,27 +13,20 @@ using namespace std;
 
 int main()
 {
-    string word1, word2;
+    char word1[1000];
+    char word2[1000];
     vector<char> common_letters;
-    while(true) {
-        getline(cin,word1);
-
-        if(word1.length() == 0)
-            break;
-
-        getline(cin,word2);
-
-        if(word2.length() == 0)
-            break;
-
-
-        for(int i=0;i<word1.length();i++) {
-            for(int j=0;j<word2.length();j++) {
-                if(word1[i] == word2[j]){
-                    common_letters.push_back(word1[i]);
-                    word2[j] = ' ';
-                    break;
+    while(gets(word1) != NULL && gets(word2) != NULL) {
+        for(int i=0;i<strlen(word1);i++) {
+            for(int j=0;j<strlen(word2);j++) {
+                if(word1[i] >= 'a' && word1[i] <= 'z'){
+                    if(word1[i] == word2[j]){
+                        common_letters.push_back(word1[i]);
+                        word2[j] = '#';
+                        break;
+                    }
                 }
+
             }
         }
 
