326e36129e6a68bbc9010555e101e577e2fac79a
cyro809 <cyro809@gmail.com>
2015-04-24 00:01:05 -0300

    Solução do Reverse and Add.

--- reverseandadd.cpp
+++ reverseandadd.cpp
@@ -1,36 +1,62 @@
 
 #include <iostream>
-#include<math.h>
 #include <cstdio>
-#include <list>
 #include <algorithm>
 #include <cstdlib>
 #include <sstream>
-#include <fstream>
-#include <string>
-#include <vector>
 
 using namespace std;
 
 int main()
 {
     int test_cases;
-    int n;
-    int n_reverse;
+    long long int n;
+    int counter = 0;
+    long long int n_reverse;
+    bool palindrome = false;
 
-    cin>>n;
 
-    string Result;          // string which will contain the result
+    cin>>test_cases;
+    for(int t=0;t<test_cases;t++){
+        cin>>n;
 
-    ostringstream convert;   // stream used for the conversion
+        while(!palindrome){
+            string Result;          // string which will contain the result
 
-    convert << n;      // insert the textual representation of 'Number' in the characters in the stream
+            ostringstream convert;   // stream used for the conversion
 
-    Result = convert.str();
-    reverse(Result.begin(), Result.end());
+            convert << n;      // insert the textual representation of 'Number' in the characters in the stream
 
-    n_reverse = atoi(Result.c_str());
+            Result = convert.str();
 
-    cout<<n_reverse<<endl;
+
+            reverse(Result.begin(), Result.end());
+
+            n_reverse = atoll(Result.c_str());
+
+            n = n + n_reverse;
+
+            convert.str("");
+
+            convert << n;      // insert the textual representation of 'Number' in the characters in the stream
+
+            Result = convert.str();
+
+
+            bool flag = true;
+            for(int i=0;i<Result.length()/2; i++) {
+                if(Result[i] != Result[Result.length()-1-i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            palindrome = flag;
+            counter++;
+        }
+        cout<<counter<<" "<<n<<endl;
+
+        counter = 0;
+        palindrome = false;
+    }
     return 0;
 }
