b29b924e43662ace52c6fe50ade0022679929fbf
cyro809 <cyro809@gmail.com>
2015-05-03 21:15:27 -0300

    Solução Largest Prime Divisor

--- largestprimedivisor.cpp
+++ largestprimedivisor.cpp
@@ -1,61 +1,76 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#define MAX 10000000
+
+int raiz_max = sqrt(MAX);
+long long primes[MAX];
+bool is_prime[MAX];
+long long nPrimes=0;
 
 using namespace std;
 
 int main()
 {
-    long long int n;
-    long long int n2;
-    int cont;
-    long long int largest_prime;
-    std::vector<int> primes;
-    primes.push_back(2);
-    for(long long int i=3; i < sqrt(100000000); i++)
+
+    memset(is_prime,true,sizeof(is_prime));
+    is_prime[1]=false;
+    is_prime[0] = false;
+    for(long long i=2;i<MAX;i++)
     {
-        bool prime=true;
-        for(int j=0;j<primes.size() && primes[j]*primes[j] <= i;j++)
+        if(is_prime[i])
         {
-            if(i % primes[j] == 0)
+            primes[nPrimes++]=i;
+            for(long long j=i*i, k=1;j<MAX;j= i*i + k++ *i)
             {
-                prime=false;
-                break;
+                is_prime[j]=false;
             }
-        }
-        if(prime)
-        {
-            primes.push_back(i);
         }
     }
+    std::vector<int> factors;
 
-    while (true) {
-            cont = 0;
-            largest_prime = 0;
-            cin>>n;
-            if(n == 0)
-                break;
+    long long n;
+    long long n2;
+    int cont;
+    while (std::cin>>n) {
 
+        cont = 0;
+
+        if(n == 0)
+            break;
+
+        if(n < 0)
+            n2 = -n;
+        else
             n2 = n;
-            for(long long int i=0; i<sqrt(n); i++){
-                if(n2 % primes[i] == 0){
-                    if(largest_prime <= primes[i]){
-                        largest_prime = primes[i];
-                    }
-                    n2 = n2/primes[i];
 
-                    if(n2 == 1) break;
-                    i=0;
-                    cont++;
+        for(long int i=0; i<nPrimes; i++){
+            if(n2 % primes[i] == 0){
+                if(std::find(factors.begin(), factors.end(), primes[i]) != factors.end()) {
+                    /* v contains x */
+                } else {
+                    factors.push_back(primes[i]);
                 }
+                if(n2/primes[i] == 1){
+                    break;
+                }
+                else{
+                    n2 = n2/primes[i];
+                }
+                i=-1;
+                cont++;
             }
-            if(cont == 0){
-                cout<<"-1"<<endl;
-            }
-            else{
-                cout<<largest_prime<<endl;
-            }
-
+        }
+        if((cont == 0) || (factors.size() == 1 && n2/factors[0] == 1)){
+            std::cout<<"-1"<<std::endl;
+        }
+        else{
+            std::cout<<n2<<std::endl;
+        }
+        factors.clear();
     }
     return 0;
 }
