1f7c330b6ceb5642d9ca897c7d8881a1351374b5
cyro809 <cyro809@gmail.com>
2015-05-12 09:42:57 -0300

    Solução Friends

--- friends.cpp
+++ friends.cpp
@@ -14,6 +14,7 @@ using namespace std;
 int *id;
 int* ranking;
 int counter;
+int *cont;
 
 int Find(int x){
     int root = x;
@@ -28,7 +29,7 @@ int Find(int x){
     return x;
 }
 
-void Union(int x, int y){
+void Union(int &x, int &y){
     int i = Find(x);
     int j = Find(y);
     if(i == j){
@@ -38,10 +39,13 @@ void Union(int x, int y){
     if(ranking[i] < ranking[j]){
         id[i] = j;
         ranking[j] += ranking[i];
+        cont[j] += cont[i];
     }
     else {
         id[j] = i;
         ranking[i] += ranking[j];
+        cont[i] += cont[j];
+        if(ranking[i] == ranking[j]) ranking[j]++;
     }
     counter--;
 }
@@ -50,7 +54,6 @@ int main()
 {
     int n, m;
     int x,y;
-    int *groups;
     int test_cases;
 
     cin>>test_cases;
@@ -58,25 +61,24 @@ int main()
         cin>>n>>m;
         id = new int[n];
         ranking = new int[n];
-        groups = new int[n];
+        cont = new int[n];
         counter = n;
         for (int i = 0; i < n; i++) {
             id[i] = i;
             ranking[i] = 0;
-            groups[i] = 0;
+            cont[i] = 1;
         }
         for(int i=0;i<m;i++){
             cin>>x>>y;
-            Union(x-1,y-1);
+            x--;
+            y--;
+            Union(x,y);
 
-        }
-        for(int i=0;i<n;i++){
-            groups[id[i]]++;
         }
         int largest_group = 0;
         for(int i=0;i<n;i++){
-            if(largest_group < groups[i]){
-                largest_group = groups[i];
+            if(largest_group < cont[i]){
+                largest_group = cont[i];
             }
         }
         cout<<largest_group<<endl;
