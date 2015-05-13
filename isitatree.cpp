10b5b79644541680f4322cac1dd5808ccc8df433
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2015-05-13 20:44:30 -0300

    Continuando solução Is is a tree

--- isitatree.cpp
+++ isitatree.cpp
@@ -19,16 +19,74 @@ vector<int> make_vector(int a)
     return v;
 }
 
+void dfs(map<int, vector<int> > &mapping, map<int, bool> &visited, int n){
+    visited[n] = true;
+
+    vector<int>::iterator it;
+    for(it = mapping[n].begin(); it != mapping[n].end(); it++){
+        if(!visited[*it]){
+            dfs(mapping,visited, *it);
+        }
+    }
+}
+
 int main()
 {
     int x,y;
+    int numRoots = 0;
+    int root = -1;
+    int test_case = 1;
+    bool isTree = true;
     map<int,bool> visited;
     map<int, int> indegree;
     map<int,vector<int> > mapping;
     while(true){
         cin>>x>>y;
+
         if(x == -1 && y == -1)
             break;
+        else if( x == 0 && y == 0){
+            map<int, int>::iterator it;
+
+            for(it = indegree.begin(); it != indegree.end(); it++) {
+                if(it->second == 0){
+                    root = it->first;
+                    numRoots++;
+                }
+                if (numRoots > 1) {
+                    isTree = false;
+                    cout<<"Case "<<test_case<<" is not a tree"<<endl;
+                    break;
+                }
+            }
+
+            if(numRoots == 0 && !isTree){
+                cout<<"Case "<<test_case<<" is not a tree"<<endl;
+            }
+            if(isTree){
+                dfs(mapping, visited, root);
+                map<int, bool>::iterator it2;
+                for( it2= visited.begin(); it2!= visited.end(); it2++){
+                    if(!it2->second){
+                        isTree = false;
+                        cout<<"Case "<<test_case<<" is not a tree"<<endl;
+                        break;
+                    }
+                }
+            }
+
+            if(isTree){
+                cout<<"Case "<<test_case<<" is a tree"<<endl;
+            }
+            mapping.clear();
+            visited.clear();
+            indegree.clear();
+            test_case++;
+            isTree = true;
+            numRoots = 0;
+            root= -1;
+
+        }
 
         if(mapping.find(x) == mapping.end()){
             mapping.insert(pair<int,vector<int> >(x, make_vector(y)));
@@ -42,10 +100,15 @@ int main()
             }
             else{
                 indegree[y]++;
+
+                if (indegree[y] > 1) {
+                    isTree = false;
+                }
             }
         }
         else{
             mapping[x].push_back(y);
+
             if(mapping.find(y) == mapping.end()){
                 vector<int> v;
                 mapping.insert(pair<int,vector<int> >(x, v));
@@ -54,20 +117,12 @@ int main()
             }
             else{
                 indegree[y]++;
+                if (indegree[y] > 1) {
+                    isTree = false;
+                }
             }
         }
 
-
-    }
-
-    map<int, vector<int> >::iterator it_type;
-    for(it_type = mapping.begin(); it_type != mapping.end(); it_type++) {
-        cout<<it_type->first<<"  adj size = "<<it_type->second.size()<<endl;
-    }
-
-    map<int, int>::iterator it;
-    for(it = indegree.begin(); it != indegree.end(); it++) {
-        cout<<it->first<<"  indegree = "<<it->second<<endl;
     }
     return 0;
 }
