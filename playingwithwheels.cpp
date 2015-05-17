777e6f75335d16b9e9548ecbf594f59edf1a3d1f
cyro809 <cyro809@gmail.com>
2015-05-17 01:08:47 -0300

    Solução BFS Playing with wheels

--- playingwithwheels.cpp
+++ playingwithwheels.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <algorithm>
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <utility>
 #include <fstream>
 #include <sstream>
@@ -14,77 +14,54 @@
 using namespace std;
 
 map<vector<int>,bool> visited;
-map<vector<int>,vector< vector<int> > > mapping;
+map<vector<int>,int > mapping;
 vector<vector<int> > forbNums;
 queue<vector<int> > q;
 int moves = 0;
 
 void bfs(vector<int> curr, vector<int> objective){
-    int currentDepth = 0;
-    int elementsToDepthIncrease = 1;
-    int nextElementsToDepthIncrease = 0;
+
     bool result = false;
 
-    visited[curr] = true;
     q.push(curr);
 
     while(!q.empty()){
+        curr.clear();
         curr = q.front();
+        q.pop();
+        if(visited[curr] == true) continue;
 
         visited[curr] = true;
+
         if(curr == objective){
             result = true;
             break;
         }
-        q.pop();
 
         for(int i=0;i<4;i++){
             vector<int> next = curr;
 
-            next[i] = curr[i]+1;
-            next[i] %=10;
-            if(find(forbNums.begin(),forbNums.end(),next) == forbNums.end()){
-                mapping[curr].push_back(next);
-                if(mapping.find(next) == mapping.end()){
-                    visited.insert(pair<vector<int>,bool>(next,false));
-                    vector<vector<int> > v;
-                    mapping.insert(pair<vector<int> ,vector<vector<int> > >(next, v));
-                }
-            }
-
-            next[i] = curr[i]+9;
+            next[i] += 1;
             next[i] %= 10;
-
-            if(find(forbNums.begin(),forbNums.end(),next) == forbNums.end()){
-                mapping[curr].push_back(next);
-                if(mapping.find(next) == mapping.end()){
-                    visited.insert(pair<vector<int>,bool>(next,false));
-                    vector<vector<int> > v;
-                    mapping.insert(pair<vector<int> ,vector<vector<int> > >(next, v));
-                }
+            if(visited[next] == false){
+                q.push(next);
             }
-        }
+            mapping[next] = mapping[curr] + 1;
 
-        nextElementsToDepthIncrease += mapping[curr].size();
+            next = curr;
+            next[i] -= 1;
+            if(next[i] == -1) next[i] = 9;
 
-        if (--elementsToDepthIncrease == 0) {
-            currentDepth++;
-            elementsToDepthIncrease = nextElementsToDepthIncrease;
-            nextElementsToDepthIncrease = 0;
-        }
-
-        vector<vector<int> >::iterator it;
-        for(it = mapping[curr].begin(); it != mapping[curr].end(); it++){
-            if(!visited[*it]){
-                q.push(*it);
-                visited[*it] = true;
+            if(visited[next] == false){
+                q.push(next);
             }
+            mapping[next] = mapping[curr] + 1;
         }
 
     }
 
     if(result){
-        cout<<currentDepth<<endl;
+        cout<<mapping[objective]<<endl;
     }
     else{
         cout<<"-1"<<endl;
@@ -105,39 +82,50 @@ int main()
     vector<int> target;
     int numForbConfig;
     vector<int> forbN;
+    int test_cases;
 
-    for(int i=0;i<4;i++){
-        int n;
-        cin>>n;
-        initConfig.push_back(n);
-    }
+    cin>>test_cases;
+    for(int t=0;t<test_cases;t++){
+        initConfig.clear();
 
-    for(int i=0;i<4;i++){
-        int n;
-        cin>>n;
-        target.push_back(n);
-    }
-
-    cin>>numForbConfig;
-
-    for(int n=0;n<numForbConfig;n++){
         for(int i=0;i<4;i++){
             int n;
             cin>>n;
-            forbN.push_back(n);
+
+            initConfig.push_back(n);
         }
-        forbNums.push_back(forbN);
+
+        mapping[initConfig] = 0;
+
+        for(int i=0;i<4;i++){
+            int n;
+            cin>>n;
+
+            target.push_back(n);
+        }
+
+        cin>>numForbConfig;
+
+        for(int n=0;n<numForbConfig;n++){
+            forbN.clear();
+            for(int i=0;i<4;i++){
+                int n;
+                cin>>n;
+                forbN.push_back(n);
+            }
+            visited[forbN] = true;
+        }
+
+        bfs(initConfig, target);
+
+        visited.clear();
+        mapping.clear();
+        queue<vector<int> > empty_q;
+        swap(q, empty_q);
+        initConfig.clear();
+        target.clear();
+        forbN.clear();
     }
 
-    bfs(initConfig, target);
-
-    visited.clear();
-    mapping.clear();
-    forbNums.clear();
-    queue<vector<int> > empty_q;
-    swap(q, empty_q);
-    initConfig.clear();
-    target.clear();
-    forbN.clear();
     return 0;
 }
